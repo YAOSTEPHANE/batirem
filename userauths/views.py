55f69f1a7a0a513dbf5375b3a4c52ee326d68ae5
kouas <YAOSTEPHANE>
2024-02-01 20:28:56 +0000

    second

--- userauths/views.py
+++ userauths/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import redirect, render
 from userauths.forms import UserRegisterForm
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from django.conf import settings
+
+
+User = settings.AUTH_USER_MODEL
 
 def register_view(request):
     
@@ -25,3 +29,16 @@ def register_view(request):
         'form': form,
     }
     return render(request, 'userauths/sign-up.html', context)
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('core:index')
+    if request.method == "POST":
+        email = request.POST.get('email') # batirem@gmail.com
+        password = request.POST.get('password') # Yk48976031
+        
+        try:
+          user = User.objects.get(email=email)
+         
+        except:
+            messages.warning(request, f"User {email} n'existe pas")
