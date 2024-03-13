39754292de9f46a797744c7464e2d76d1b09ef0d
YAOSTEPHANE <kouassistephane489@gmail.com>
2024-03-13 12:49:11 +0000

    mern

--- userauths/views.py
+++ userauths/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import redirect, render
-from userauths.forms import UserRegisterForm
+from userauths.forms import UserRegisterForm, ProfileForm
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from django.conf import settings
+from userauths.models import Profile, User
 
 
-User = settings.AUTH_USER_MODEL
-
+# User = settings.AUTH_USER_MODEL
 
 def register_view(request):
     
@@ -14,8 +14,8 @@ def register_view(request):
         form = UserRegisterForm(request.POST or None)
         if form.is_valid():
             new_user = form.save()
-            username = form.cleaned_data.get("nom utilisateur")
-            messages.success(request, f"Hey {username}, Ton compte a été crée avec succes.")
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Hey {username}, You account was created successfully.")
             new_user = authenticate(username=form.cleaned_data['email'],
                                     password=form.cleaned_data['password1']
             )
@@ -33,7 +33,7 @@ def register_view(request):
 
 def login_view(request):
     if request.user.is_authenticated:
-        messages.warning(request, f"Hé vous etes deja connecté.")
+        messages.warning(request, f"Hey you are already Logged In.")
         return redirect("core:index")
     
     if request.method == "POST":
@@ -46,13 +46,13 @@ def login_view(request):
 
             if user is not None:
                 login(request, user)
-                messages.success(request, "Vous etes connecté.")
+                messages.success(request, "You are logged in.")
                 return redirect("core:index")
             else:
-                messages.warning(request, "L'utilisateur n'existe pas creer un compte.")
+                messages.warning(request, "User Does Not Exist, create an account.")
     
         except:
-            messages.warning(request, f"Utilisateur avec {email} n'existe pas")
+            messages.warning(request, f"User with {email} does not exist")
         
 
     
@@ -63,5 +63,26 @@ def login_view(request):
 def logout_view(request):
 
     logout(request)
-    messages.success(request, "Vous etes deconnecté.")
+    messages.success(request, "You logged out.")
     return redirect("userauths:sign-in")
+
+
+def profile_update(request):
+    profile = Profile.objects.get(user=request.user)
+    if request.method == "POST":
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            new_form = form.save(commit=False)
+            new_form.user = request.user
+            new_form.save()
+            messages.success(request, "Profile Updated Successfully.")
+            return redirect("core:dashboard")
+    else:
+        form = ProfileForm(instance=profile)
+
+    context = {
+        "form": form,
+        "profile": profile,
+    }
+
+    return render(request, "userauths/profile-edit.html", context)
