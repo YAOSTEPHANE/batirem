e1d53bdafd6b787735feea27cf5b7ac05557cecc
YAOSTEPHANE <106774656+YAOSTEPHANE@users.noreply.github.com>
2024-03-18 14:00:59 +0000

    improve

--- userauths/apps.py
+++ userauths/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
 
-
 class UserauthsConfig(AppConfig):
+    """Configuration for the userauths app."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'userauths'
+
+    def ready(self):
+        """Override the ready method to perform any post-import setup."""
+        pass
