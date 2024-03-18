b1357c19754daf33dfca0cef263d62877a6c64fd
YAOSTEPHANE <106774656+YAOSTEPHANE@users.noreply.github.com>
2024-03-18 14:00:59 +0000

    improve

--- useradmin/urls.py
+++ useradmin/urls.py
@@ -4,9 +4,10 @@ from useradmin import views
 app_name = "useradmin"
 
 urlpatterns = [
-    path("", views.dashboard, name="dashboard"),
-    path("products/", views.dashboard_products, name="dashboard-products"),
-    path("add-products/", views.dashboard_add_product, name="dashboard-add-products"),
-    path("edit-products/<pid>/", views.dashboard_edit_product, name="dashboard-edit-products"),
-    path("delete-products/<pid>/", views.dashboard_delete_product, name="dashboard-delete-products"),
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path("dashboard/products/", views.dashboard_products, name="dashboard-products"),
+    path("dashboard/add-products/", views.dashboard_add_product, name="dashboard-add-products"),
+    path("dashboard/edit-products/<int:pid>/", views.dashboard_edit_product, name="dashboard-edit-products"),
+    path("dashboard/delete-products/<int:pid>/", views.dashboard_delete_product, name="dashboard-delete-products"),
 ]
+
