c35c2614c607060192510a38c0acaad08d721525
YAOSTEPHANE <106774656+YAOSTEPHANE@users.noreply.github.com>
2024-03-18 14:00:41 +0000

    improve

--- ecomprj/urls.py
+++ ecomprj/urls.py
@@ -11,25 +11,4 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-
-from django.conf import settings
-from django.conf.urls.static import static
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("core.urls")),
-    path("user/", include("userauths.urls")),
-    path("useradmin/", include("useradmin.urls")),
-
-    # path("ckeditor/", include("ckeditor_uploader.urls")),
-    path("ckeditor5/", include('django_ckeditor_5.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
